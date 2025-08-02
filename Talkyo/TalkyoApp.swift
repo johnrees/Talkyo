1cfa9bce7ba720d85f62e005825f603ed04cb58d
John Rees <john@bitsushi.com>
2025-08-02 16:17:11 +0100

    Initial Commit

---
+++ Talkyo/TalkyoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkyoApp.swift
+//  Talkyo
+//
+//  Created by John Rees on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct TalkyoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
